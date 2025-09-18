3f646785717666e4a51f9d8968e342d5c07761cb
cxrala <ameliang123@gmail.com>
2025-09-19 00:42:20 +0100

    feat: play around with react a bit and create a simple form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const form = e.currentTarget
+    const formData = new FormData(form)
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson.username);
+
+    setName("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Prompt: butterflies</p>
+      <form method="post" onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          name="username"
+          value={name}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value) }
+          placeholder="Enter your name"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
